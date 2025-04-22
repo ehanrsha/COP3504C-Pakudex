b03a875aaef54eb044111e486d7cd94a14f6d21a
IasoInc <iasox.inc@gmail.com>
2025-04-22 17:12:22 -0400

    work on pakuri_program

--- pakuri_program.py
+++ pakuri_program.py
@@ -1,3 +1,5 @@
+list = []
+
 
 def main() :
     print("""
@@ -22,10 +24,21 @@ Pakudex Main Menu
      
         """)
 
+
     userInput = input('What would you like to do?')
 
     if userInput == 1 :
+        if (len(list) == 0):
+            print("No Pakuri in Pakudex yet!")
+        else :
+            for index, pakuri in enumerate(list) :
+                print(f"{index}. {pakuri}")
 
+    elif userInput == 2:
+        userInput = input("Enter the name of the species to display: ")
+        print()
+
+    #still building here, gonna work on pakuri first though
 
 
 main()
