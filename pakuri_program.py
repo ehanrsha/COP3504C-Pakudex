03941a6a62835252d06f155ac7550dc7c45e11c5
IasoInc <iasox.inc@gmail.com>
2025-04-22 16:57:58 -0400

    first commit

---
+++ pakuri_program.py
@@ -0,0 +1,31 @@
+
+def main() :
+    print("""
+Welcome to Pakudex: Tracker Extraordinaire!
+Enter max capacity of the Pakudex: 30
+The Pakudex can hold 30 species of Pakuri. 
+
+    """)
+
+    keepRunning = True
+
+    while keepRunning :
+        print ("""
+Pakudex Main Menu
+-----------------
+1. List Pakuri
+2. Show Pakuri
+3. Add Pakuri
+4. Evolve Pakuri
+5. Sort Pakuri
+6. Exit
+     
+        """)
+
+    userInput = input('What would you like to do?')
+
+    if userInput == 1 :
+
+
+main()
+#hello
