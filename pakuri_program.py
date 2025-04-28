d7867aa5c3cb022a7f0a494b47c7a0e7fba30ef4
Oliver Deng <oliver.t.deng@gmail.com>
2025-04-28 16:36:35 -0400

    Fix loop

--- pakuri_program.py
+++ pakuri_program.py
@@ -3,19 +3,17 @@ from pakudex import Pakudex
 
 
 def main():
-    print(
-        """
-Welcome to Pakudex: Tracker Extraordinaire!
-Enter max capacity of the Pakudex: 30
-The Pakudex can hold 30 species of Pakuri. 
-
-    """
+    capacity: int = int(
+        input(
+            " Welcome to Pakudex: Tracker Extraordinaire! Enter max capacity of the Pakudex: "
+        )
     )
+    print(f"The Pakudex can hold {capacity} species of Pakuri.")
 
-    keepRunning = True
-    pakudex = Pakudex()
+    running = True
+    pakudex = Pakudex(capacity)
 
-    while keepRunning:
+    while running:
         print(
             """
 Pakudex Main Menu
@@ -28,62 +26,64 @@ Pakudex Main Menu
 6. Exit"""
         )
 
-    userInput = input("What would you like to do?")
-    try:
-        userInput = int(userInput)
+        userInput = input("What would you like to do? ")
+        try:
+            userInput = int(userInput)
 
-    except Exception:
-        print("What would you like to do? Unrecogniezd menu selection!\n")
-        pass
+        except Exception:
+            print("Unrecogniezd menu selection!\n")
+            pass
 
-    match userInput:
-        case 1:
-            if not pakudex.pakuri_list:
-                print("No Pakuri in Pakudex yet!")
-            else:
-                for index, pakuri in enumerate(pakudex.pakuri_list):
-                    print(f"{index}. {pakuri}")
+        match userInput:
+            case 1:
+                if not pakudex.pakuri_list:
+                    print("No Pakuri in Pakudex yet!")
+                else:
+                    for index, pakuri in enumerate(pakudex.pakuri_list):
+                        print(f"{index}. {pakuri}")
 
-        case 2:
-            userInput = input("Enter the name of the species to display: ")
+            case 2:
+                userInput = input("Enter the name of the species to display: ")
 
-            dataList: list[int] | None = pakudex.get_stats(userInput)
-            if dataList is None:
-                print("Error: No such Pakuri!")
-            else:
-                print()
-                print(f"Species: {userInput}")
-                print(f"Attack: {dataList[0]}")
-                print(f"Defense: {dataList[1]}")
-                print(f"Speed: {dataList[2]}")
+                dataList: list[int] | None = pakudex.get_stats(userInput)
+                if dataList is None:
+                    print("Error: No such Pakuri!")
+                else:
+                    print()
+                    print(f"Species: {userInput}")
+                    print(f"Attack: {dataList[0]}")
+                    print(f"Defense: {dataList[1]}")
+                    print(f"Speed: {dataList[2]}")
 
-        case 3:
-            userInput = input("Enter the name of the species to add: ")
-            newPakuri = Pakuri(userInput)
-            if newPakuri in pakudex.pakuri_list:
-                print("Error: Pakudex already contains this species!")
-            elif len(pakudex.pakuri_list) == pakudex.capacity:
-                print("Error: Pakudex is full!")
-            else:
-                print(f"Pakuri species {newPakuri.get_species()} successfully added!")
+            case 3:
+                userInput = input("Enter the name of the species to add: ")
+                newPakuri = Pakuri(userInput)
+                if newPakuri in pakudex.pakuri_list:
+                    print("Error: Pakudex already contains this species!")
+                elif len(pakudex.pakuri_list) == pakudex.capacity:
+                    print("Error: Pakudex is full!")
+                else:
+                    print(
+                        f"Pakuri species {newPakuri.get_species()} successfully added!"
+                    )
 
-        case 4:
-            userInput = input("Enter the name of the species to evolve: ")
-            if pakudex.evolve_species(Pakuri(userInput)):
-                print(f"{userInput} has evolved!")
-            else:
-                print("Error: No such Pakuri!")
+            case 4:
+                userInput = input("Enter the name of the species to evolve: ")
+                if pakudex.evolve_species(Pakuri(userInput)):
+                    print(f"{userInput} has evolved!")
+                else:
+                    print("Error: No such Pakuri!")
 
-        case 5:
-            userInput = input("Enter the name of the species to display: ")
-            print("Pakuri have been sorted!")
+            case 5:
+                userInput = input("Enter the name of the species to display: ")
+                print("Pakuri have been sorted!")
 
-        case 6:
-            print("Thanks for using Pakudex! Bye!")
-            keepRunning = False
+            case 6:
+                print("Thanks for using Pakudex! Bye!")
+                running = False
 
-        case _:
-            print("What would you like to do? Unrecognized menu selection!\n")
+            case _:
+                print("What would you like to do? Unrecognized menu selection!\n")
 
 
 if __name__ == "__main__":
