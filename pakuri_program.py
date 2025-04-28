d2f50b80e91a6e3a6d1b6b8488d4c2647fbecb5e
Oliver Deng <oliver.t.deng@gmail.com>
2025-04-28 15:10:54 -0400

    Attempt 2

--- pakuri_program.py
+++ pakuri_program.py
@@ -43,19 +43,48 @@ Pakudex Main Menu
             else:
                 for index, pakuri in enumerate(pakudex.pakuri_list):
                     print(f"{index}. {pakuri}")
+
         case 2:
             userInput = input("Enter the name of the species to display: ")
+            newPakuri = Pakuri(userInput)
+
+            dataList: list[int] | None = pakudex.get_stats(newPakuri)
+            if dataList is None:
+                print("Error: No such Pakuri!")
+            else:
+                print()
+                print(f"Species: {userInput}")
+                print(f"Attack: {dataList[0]}")
+                print(f"Defense: {dataList[1]}")
+                print(f"Speed: {dataList[2]}")
+
         case 3:
             userInput = input("Enter the name of the species to add: ")
+            newPakuri = Pakuri(userInput)
+            if newPakuri in pakudex.pakuri_list:
+                print("Error: Pakudex already contains this species!")
+            elif len(pakudex.pakuri_list) == pakudex.capacity:
+                print("Error: Pakudex is full!")
+            else:
+                print(f"Pakuri species {newPakuri.get_species()} successfully added!")
+
         case 4:
             userInput = input("Enter the name of the species to evolve: ")
+            if pakudex.evolve_species(Pakuri(userInput)):
+                print(f"{userInput} has evolved!")
+            else:
+                print("Error: No such Pakuri!")
+
         case 5:
             userInput = input("Enter the name of the species to display: ")
+            print("Pakuri have been sorted!")
+
         case 6:
-            pakudex.sort_pakuri()
+            print("Thanks for using Pakudex! Bye!")
+            keepRunning = False
+
         case _:
             print("What would you like to do? Unrecognized menu selection!\n")
-            pass
 
 
 if __name__ == "__main__":
